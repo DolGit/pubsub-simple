50473da0ef11d1954678d41aed6a9be86009e9cb
Alex Dollery <alexdollery@yahoo.co.uk>
2021-02-03 14:21:05 +0000

    typo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-const subs = {}
+export default () => {
+  const subs = {}
 
-const get = () => subs
+  const get = () => subs
 
-const call = (key) => (args) => {
-  subs[key] && subs[key].map((fn) => fn.apply(this, args))
+  const call = (key) => (args) => {
+    subs[key] && subs[key].map((fn) => fn.apply(this, args))
+  }
+
+  const add = (key) => (fn) => {
+    const sub = subs[key] = subs[key] || []
+    fn && sub.push(fn)
+    return subs
+  }
+
+  const define = (key) => add(key)()
+
+  return { get, call, add, define }
 }
-
-const add = (key) => (fn) => {
-  const sub = subs[key] = subs[key] || []
-  fn && sub.push(fn)
-  return subs
-}
-
-const define = (key) => add(key)()
-
-export { get, call, add, define }
