c4838f60b827723bd091740be713bf499a2b9792
Alex Dollery <alexdollery@yahoo.co.uk>
2021-02-03 18:28:18 +0000

    re-ran build

--- es/index.js
+++ es/index.js
@@ -1,29 +1,31 @@
 var _this = this;
 
-var subs = {};
+export default (function () {
+  var subs = {};
 
-var get = function get() {
-  return subs;
-};
-
-var call = function call(key) {
-  return function (args) {
-    subs[key] && subs[key].map(function (fn) {
-      return fn.apply(_this, args);
-    });
-  };
-};
-
-var add = function add(key) {
-  return function (fn) {
-    var sub = subs[key] = subs[key] || [];
-    fn && sub.push(fn);
+  var get = function get() {
     return subs;
   };
-};
 
-var define = function define(key) {
-  return add(key)();
-};
+  var call = function call(key) {
+    return function (args) {
+      subs[key] && subs[key].map(function (fn) {
+        return fn.apply(_this, args);
+      });
+    };
+  };
 
-export { get, call, add, define };
+  var add = function add(key) {
+    return function (fn) {
+      var sub = subs[key] = subs[key] || [];
+      fn && sub.push(fn);
+      return subs;
+    };
+  };
+
+  var define = function define(key) {
+    return add(key)();
+  };
+
+  return { get: get, call: call, add: add, define: define };
+});
